877982116162d3d17108a8c740b29bda70c8258f
Nemanja Stefanovic <stefanemanja@gmail.com>
2017-06-26 15:54:45 +0200

    Vezba zakljucno sa devetom lekcijom

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Application</title>
+</head>
+<body>
+
+	@yield('content')
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+
+	<ul>
+	
+		@foreach ($tasks as $task)
+			
+			<li>{{$task->body}}</li>
+
+		@endforeach
+	
+	</ul>		
+
+
+
+</body>
+</html>
